373f6a7079f5a10be95b4fcaaec027c974ef884f
bakeza <dbakeza2002@gmail.com>
2024-03-02 18:56:40 +0200

    Create repo

---
+++ src/main/java/com/wellcare/wellcare/WellcareApplication.java
@@ -0,0 +1,13 @@
+package com.wellcare.wellcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellcareApplication.class, args);
+	}
+
+}
