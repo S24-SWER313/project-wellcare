125f2444da12d7b61f482e23fa4435f44401564c
bakeza <dbakeza2002@gmail.com>
2024-03-04 00:27:42 +0200

    Add User registration

--- src/main/java/com/wellcare/wellcare/Repositories/UserRepository.java
+++ src/main/java/com/wellcare/wellcare/Repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.wellcare.wellcare.Repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.*;
+
 import com.wellcare.wellcare.Models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,11 +15,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     public Optional<User> findByUsername(String username);
 
-    @Query("SELECT u FROM User u WHERE u.id IN :user") 
+    @Query("SELECT u FROM User u WHERE u.id IN :user")
     public List<User> findAllByUserId(@Param("user") List<Long> userId);
-    
+
     @Query("SELECT DISTINCT u FROM User u WHERE u.username LIKE %:query% OR u.email LIKE %:query%")
     public List<User> findBySearch(@Param("query") String query);
 
-    
 }
