a757b6eaef87bf2ae7af19c119f4716f60887c72
bakeza <dbakeza2002@gmail.com>
2024-03-18 22:43:46 +0200

    Fix update profile

--- src/main/java/com/wellcare/wellcare/Controllers/UserController.java
+++ src/main/java/com/wellcare/wellcare/Controllers/UserController.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.wellcare.wellcare.Models.User;
 import com.wellcare.wellcare.Repositories.UserRepository;
+import com.wellcare.wellcare.Security.services.UserDetailsImpl;
 
 import jakarta.transaction.Transactional;
 
@@ -22,6 +26,8 @@ import jakarta.transaction.Transactional;
 public class UserController {
 
     @Autowired
+    PasswordEncoder encoder;
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/profile/{userId}")
@@ -29,7 +35,7 @@ public class UserController {
         Optional<User> userOptional = userRepository.findById(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
-            user.getSavedPost().size(); // Initialize the collection
+            user.getSavedPost().size();
             return ResponseEntity.ok().body(user);
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
@@ -39,6 +45,15 @@ public class UserController {
     @PutMapping("/profile/{userId}")
     @Transactional
     public ResponseEntity<?> updateUserProfile(@PathVariable Long userId, @RequestBody User updatedUser) {
+        // Get the authenticated user
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        // Check if the authenticated user ID matches the requested user ID
+        if (!userDetails.getId().equals(userId)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You are not authorized to update this profile");
+        }
+
         Optional<User> existingUser = userRepository.findById(userId);
         if (existingUser.isPresent()) {
             User user = existingUser.get();
@@ -51,6 +66,13 @@ public class UserController {
             user.setBio(updatedUser.getBio());
             user.setGender(updatedUser.getGender());
             user.setImage(updatedUser.getImage());
+
+            // Check if the password field is provided and update password if necessary
+            if (updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()) {
+                String hashedPassword = encoder.encode(updatedUser.getPassword()); // Hash the password
+                user.setPassword(hashedPassword);
+            }
+
             userRepository.save(user);
 
             return ResponseEntity.ok().body("User profile updated successfully");
