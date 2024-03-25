0c48f251e32558d8838825401411f085b6539b95
bakeza <dbakeza2002@gmail.com>
2024-03-25 11:23:16 +0200

    Validate Post and Like

--- src/main/java/com/wellcare/wellcare/Models/Like.java
+++ src/main/java/com/wellcare/wellcare/Models/Like.java
@@ -2,31 +2,36 @@ package com.wellcare.wellcare.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "likes")
 public class Like {
-    private @Id @GeneratedValue Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User author;
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User author;
 
-    @ManyToOne
-    @JoinColumn(name = "post_id")
-    private Post post;
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name = "post_id")
+  private Post post;
 
-    public Like() {
-    }
+  public Like() {}
 
-    public Like(Post post, User author) {
-        this.post = post;
-        this.author = author;
-    }
+  public Like(Post post, User author) {
+    this.post = post;
+    this.author = author;
+  }
 }
--- src/main/java/com/wellcare/wellcare/Models/Post.java
+++ src/main/java/com/wellcare/wellcare/Models/Post.java
@@ -6,18 +6,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -35,33 +36,43 @@ import lombok.Data;
 @Table(name = "post")
 public class Post {
 
-    @Id @GeneratedValue 
+    @Id
+    @GeneratedValue
     @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @JsonProperty("location")
+    @Size(max = 255)
     private String location;
 
+    @NotBlank
+    @Size(max = 255)
     private String content;
 
     private List<String> attachment;
 
-    private Integer noOfLikes = 0 ;
+    @NotNull
+    @Min(0)
+    private Integer noOfLikes = 0;
 
+    @NotNull
+    @Min(0)
     private Integer noOfComments = 0;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "user_id")
-    @JsonIgnoreProperties({"firstName", "lastName","password", "email", "mobile", "bio", "gender", "image", "role", "follower", "following", "savedPost"})
+    @JsonIgnoreProperties({ "firstName", "lastName", "password", "email", "mobile", "bio", "gender", "image", "role",
+            "follower", "following", "savedPost" })
     private User user;
-
-    
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "post_likes", joinColumns = @JoinColumn(name = "post_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
-    @JsonIgnoreProperties({"firstName", "lastName","password", "email", "mobile", "bio", "gender", "image", "role", "follower", "following", "savedPost"})
+    @JsonIgnoreProperties({ "firstName", "lastName", "password", "email", "mobile", "bio", "gender", "image", "role",
+            "follower", "following", "savedPost" })
     private Set<User> likes = new HashSet<>();
 
     @OneToMany(mappedBy = "post", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
@@ -70,14 +81,14 @@ public class Post {
 
     public Post() {
         this.createdAt = LocalDateTime.now();
-        this.noOfLikes = 0 ;
+        this.noOfLikes = 0;
     }
 
     public Post(String content) {
         this.content = content;
         this.createdAt = LocalDateTime.now();
         this.noOfLikes = 0;
-        this.noOfComments = 0 ;
+        this.noOfComments = 0;
     }
 
     public Post(String location, List<String> attachment) {
